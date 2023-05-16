186140b84d0727d41b33ba33ca87c5d4cefc0885
shrouk gaber <shroukgaber281@yahoo.com>
2023-05-16 00:22:57 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,11 +10,13 @@ char *str_concat(char *s1, char *s2)
 {
 	char *conct;
 	int i, ci;
-if (s1 == NULL)
+
+	if (s1 == NULL)
+
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-		i = ci = 0;
+i = ci = 0;
 	while (s1[i] != '\0')
 		i++;
 	while (s2[ci] != '\0')
@@ -23,16 +25,17 @@ if (s1 == NULL)
 	if (conct == NULL)
 		return (NULL);
 	i = ci = 0;
-while (s1[i] != '\0')
+	while (s1[i] != '\0')
 	{
 		conct[i] = s1[i];
-i++;
+		i++;
 	}
-while (s2[ci] != '\0')
+	while (s2[ci] != '\0')
 	{
 		conct[i] = s2[ci];
-i++, ci++;
+		i++, ci++;
 	}
-conct[i] = '\0';
-return (conct);
+	conct[i] = '\0';
+	return (conct);
 }
+
